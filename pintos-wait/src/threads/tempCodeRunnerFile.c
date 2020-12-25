7e531c209af0a4557d26fa2cbc85ed48eee26651
CanisZhang <60993363+CanisZhang@users.noreply.github.com>
2020-12-25 22:53:45 +0800

    解决忙等待

---
+++ pintos-wait/src/threads/tempCodeRunnerFile.c
@@ -0,0 +1,7 @@
+/* Schedules a new process.  At entry, interrupts must be off and
+   the running process's state must have been changed from
+   running to some other state.  This function finds another
+   thread to run and switches to it.
+
+   It's not safe to call printf() until thread_schedule_tail()
+   has completed. */
